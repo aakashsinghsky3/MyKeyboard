06617fd65f5a187fc96661662e5dab762a2d9a76
Aakash Singh <aakashsinghsky3@gmail.com>
2026-09-02 12:44:58 +0530

    Version 12.0: Expanded English/Hinglish asset dictionary to over 860,000+ words, bringing total keyboard dictionary size to over 1.4 MILLION words!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aakashworkcontact.keyboard"
         minSdk = 24
         targetSdk = 37
-        versionCode = 110
-        versionName = "11.0"
+        versionCode = 120
+        versionName = "12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
