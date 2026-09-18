e3cdf9c9975e8871d63819b08492e969fe68b497
Aakash Singh <aakashsinghsky3@gmail.com>
2026-09-18 11:24:22 +0530

    Bump version to 13.14 (144) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aakashworkcontact.keyboard"
         minSdk = 24
         targetSdk = 37
-        versionCode = 143
-        versionName = "13.13"
+        versionCode = 144
+        versionName = "13.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
